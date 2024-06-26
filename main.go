aef75573369b3baf3ceae57f51c8342e50e320a5
Joey Chilson <joeychilson@outlook.com>
2024-06-26 02:12:37 -0500

    add options to exclude dirs and filter by ext

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing/object"
@@ -14,30 +15,36 @@ import (
 func main() {
 	repoURL := flag.String("repo", "", "URL of the Git repository")
 	destFolder := flag.String("dest", "", "Destination folder for flattened files")
+	excludeDirs := flag.String("exclude", "", "Comma-separated list of directories to exclude")
+	extensions := flag.String("ext", "", "Comma-separated list of file extensions to include (e.g., .go,.txt)")
 
 	flag.Parse()
 
 	if *repoURL == "" || *destFolder == "" {
-		fmt.Println("Usage: gitflat -repo <repository_url> -dest <destination_folder>")
+		fmt.Println("Usage: gitflat -repo <repository_url> -dest <destination_folder> [-exclude <dir1,dir2,...>] [-ext <.ext1,.ext2,...>]")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
 	err := flatten(&options{
-		RepoURL:    *repoURL,
-		DestFolder: *destFolder,
+		RepoURL:     *repoURL,
+		DestFolder:  *destFolder,
+		ExcludeDirs: strings.Split(*excludeDirs, ","),
+		Extensions:  strings.Split(*extensions, ","),
 	})
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("All files from %s have been flattened to %s\n", *repoURL, *destFolder)
+	fmt.Printf("Selected files from %s have been flattened to %s\n", *repoURL, *destFolder)
 }
 
 type options struct {
-	RepoURL    string
-	DestFolder string
+	RepoURL     string
+	DestFolder  string
+	ExcludeDirs []string
+	Extensions  []string
 }
 
 func flatten(opts *options) error {
@@ -65,6 +72,14 @@ func flatten(opts *options) error {
 	}
 
 	err = tree.Files().ForEach(func(f *object.File) error {
+		if shouldExclude(f.Name, opts.ExcludeDirs) {
+			return nil
+		}
+
+		if !hasValidExtension(f.Name, opts.Extensions) {
+			return nil
+		}
+
 		content, err := f.Contents()
 		if err != nil {
 			return err
@@ -97,4 +112,26 @@ func flatten(opts *options) error {
 	}
 
 	return nil
+}
+
+func shouldExclude(path string, excludeDirs []string) bool {
+	for _, dir := range excludeDirs {
+		if dir != "" && strings.HasPrefix(path, dir) {
+			return true
+		}
+	}
+	return false
+}
+
+func hasValidExtension(path string, extensions []string) bool {
+	if len(extensions) == 0 || (len(extensions) == 1 && extensions[0] == "") {
+		return true
+	}
+	ext := filepath.Ext(path)
+	for _, validExt := range extensions {
+		if validExt != "" && ext == validExt {
+			return true
+		}
+	}
+	return false
 }
