5e83e54ecbec27774ee9138fe9326ce4c0cb0ab2
Joey Chilson <joeychilson@outlook.com>
2024-07-01 01:22:19 -0500

    Add single file support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # gitflat
 
-A simple tool for flattening a git repository to a single directory.
+A simple tool for flattening a git repository to a single directory or file.
 
 ## Installation
 
@@ -11,5 +11,5 @@ go install github.com/joeychilson/gitflat
 ## Usage
 
 ```bash
-gitflat -repo <repository_url> -dest <destination_folder>
+gitflat -repo <repository_url> -dest <destination_folder> [-exclude <dir1,dir2,...> [-include <dir>] [-exts <.ext1,.ext2,...>] [-single]
 ```
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
@@ -12,39 +13,57 @@ import (
 	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
+type options struct {
+	RepoURL     string
+	DestFolder  string
+	ExcludeDirs []string
+	Include     string
+	Extensions  []string
+	SingleFile  bool
+}
+
 func main() {
 	repoURL := flag.String("repo", "", "URL of the Git repository")
 	destFolder := flag.String("dest", "", "Destination folder for flattened files")
 	excludeDirs := flag.String("exclude", "", "Comma-separated list of directories to exclude")
-	extensions := flag.String("ext", "", "Comma-separated list of file extensions to include (e.g., .go,.txt)")
+	include := flag.String("include", "", "Only include files from this directory")
+	exts := flag.String("exts", "", "Comma-separated list of file extensions to include (e.g., .go,.txt)")
+	singleFile := flag.Bool("single", false, "Flatten the repo into a single text file")
 
 	flag.Parse()
 
 	if *repoURL == "" || *destFolder == "" {
-		fmt.Println("Usage: gitflat -repo <repository_url> -dest <destination_folder> [-exclude <dir1,dir2,...>] [-ext <.ext1,.ext2,...>]")
+		fmt.Println("Usage: gitflat -repo <repository_url> -dest <destination_folder> [-exclude <dir1,dir2,...>] [-include <dir>] [-exts <.ext1,.ext2,...>] [-single]")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	err := flatten(&options{
+	opts := &options{
 		RepoURL:     *repoURL,
 		DestFolder:  *destFolder,
-		ExcludeDirs: strings.Split(*excludeDirs, ","),
-		Extensions:  strings.Split(*extensions, ","),
-	})
+		ExcludeDirs: strings.Fields(*excludeDirs),
+		Include:     *include,
+		Extensions:  strings.Fields(*exts),
+		SingleFile:  *singleFile,
+	}
+
+	var err error
+	if opts.SingleFile {
+		err = flattenToSingleFile(opts)
+	} else {
+		err = flatten(opts)
+	}
+
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("Selected files from %s have been flattened to %s\n", *repoURL, *destFolder)
-}
-
-type options struct {
-	RepoURL     string
-	DestFolder  string
-	ExcludeDirs []string
-	Extensions  []string
+	if opts.SingleFile {
+		fmt.Printf("Selected files from %s have been flattened to a single file in %s\n", *repoURL, *destFolder)
+	} else {
+		fmt.Printf("Selected files from %s have been flattened to %s\n", *repoURL, *destFolder)
+	}
 }
 
 func flatten(opts *options) error {
@@ -53,26 +72,83 @@ func flatten(opts *options) error {
 		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
 	})
 	if err != nil {
-		return fmt.Errorf("error cloning repository: %v", err)
+		return fmt.Errorf("error cloning repository: %w", err)
 	}
 
 	ref, err := repo.Head()
 	if err != nil {
-		return fmt.Errorf("error getting HEAD: %v", err)
+		return fmt.Errorf("error getting HEAD: %w", err)
 	}
 
 	commit, err := repo.CommitObject(ref.Hash())
 	if err != nil {
-		return fmt.Errorf("error getting commit: %v", err)
+		return fmt.Errorf("error getting commit: %w", err)
 	}
 
 	tree, err := commit.Tree()
 	if err != nil {
-		return fmt.Errorf("error getting tree: %v", err)
+		return fmt.Errorf("error getting tree: %w", err)
 	}
 
-	err = tree.Files().ForEach(func(f *object.File) error {
-		if shouldExclude(f.Name, opts.ExcludeDirs) {
+	err = processFiles(tree, opts, nil)
+	if err != nil {
+		return fmt.Errorf("error processing files: %w", err)
+	}
+
+	err = cleanupDirectories(opts.DestFolder)
+	if err != nil {
+		return fmt.Errorf("error removing directories: %w", err)
+	}
+
+	return nil
+}
+
+func flattenToSingleFile(opts *options) error {
+	repo, err := git.PlainClone(opts.DestFolder, false, &git.CloneOptions{
+		URL:               opts.RepoURL,
+		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+	})
+	if err != nil {
+		return fmt.Errorf("error cloning repository: %w", err)
+	}
+
+	ref, err := repo.Head()
+	if err != nil {
+		return fmt.Errorf("error getting HEAD: %w", err)
+	}
+
+	commit, err := repo.CommitObject(ref.Hash())
+	if err != nil {
+		return fmt.Errorf("error getting commit: %w", err)
+	}
+
+	tree, err := commit.Tree()
+	if err != nil {
+		return fmt.Errorf("error getting tree: %w", err)
+	}
+
+	outputFile, err := os.Create(filepath.Join(opts.DestFolder, "flattened_repo.txt"))
+	if err != nil {
+		return fmt.Errorf("error creating output file: %w", err)
+	}
+	defer outputFile.Close()
+
+	err = processFiles(tree, opts, outputFile)
+	if err != nil {
+		return fmt.Errorf("error processing files: %w", err)
+	}
+
+	err = cleanupDirectories(opts.DestFolder)
+	if err != nil {
+		return fmt.Errorf("error cleaning up directory: %w", err)
+	}
+
+	return nil
+}
+
+func processFiles(tree *object.Tree, opts *options, outputWriter io.Writer) error {
+	return tree.Files().ForEach(func(f *object.File) error {
+		if shouldExclude(f.Name, opts.ExcludeDirs, opts.Include) {
 			return nil
 		}
 
@@ -82,39 +158,42 @@ func flatten(opts *options) error {
 
 		content, err := f.Contents()
 		if err != nil {
-			return err
+			return fmt.Errorf("error reading file contents: %w", err)
 		}
 
-		targetPath := filepath.Join(opts.DestFolder, filepath.Base(f.Name))
-
-		err = os.WriteFile(targetPath, []byte(content), 0644)
+		if opts.SingleFile {
+			_, err = fmt.Fprintf(outputWriter, "--- %s ---\n%s\n\n", f.Name, content)
+		} else {
+			targetPath := filepath.Join(opts.DestFolder, filepath.Base(f.Name))
+			err = os.WriteFile(targetPath, []byte(content), 0644)
+		}
 		if err != nil {
-			return err
+			return fmt.Errorf("error writing file: %w", err)
 		}
 		return nil
 	})
-	if err != nil {
-		return fmt.Errorf("error processing files: %v", err)
-	}
+}
 
-	err = filepath.Walk(opts.DestFolder, func(path string, info fs.FileInfo, err error) error {
+func cleanupDirectories(destFolder string) error {
+	return filepath.Walk(destFolder, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-		if info.IsDir() && path != opts.DestFolder {
-			os.RemoveAll(path)
+		if path != destFolder && info.IsDir() {
+			err := os.RemoveAll(path)
+			if err != nil {
+				return fmt.Errorf("error removing directory: %w", err)
+			}
 			return filepath.SkipDir
 		}
 		return nil
 	})
-	if err != nil {
-		return fmt.Errorf("error removing directories: %v", err)
-	}
-
-	return nil
 }
 
-func shouldExclude(path string, excludeDirs []string) bool {
+func shouldExclude(path string, excludeDirs []string, include string) bool {
+	if include != "" {
+		return !strings.HasPrefix(path, include)
+	}
 	for _, dir := range excludeDirs {
 		if dir != "" && strings.HasPrefix(path, dir) {
 			return true
@@ -127,9 +206,8 @@ func hasValidExtension(path string, extensions []string) bool {
 	if len(extensions) == 0 || (len(extensions) == 1 && extensions[0] == "") {
 		return true
 	}
-	ext := filepath.Ext(path)
 	for _, validExt := range extensions {
-		if validExt != "" && ext == validExt {
+		if validExt != "" && strings.HasSuffix(path, validExt) {
 			return true
 		}
 	}
